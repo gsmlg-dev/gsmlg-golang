ed7dc482f1f2a66b9c28a54cbdad0937d77cb339
Jonathan Gao <gsmlg.com@gmail.com>
2022-06-30 00:58:20 +0800

    fix: Fix geoip ProduceCity.

--- geoip2/produce.go
+++ geoip2/produce.go
@@ -21,14 +21,28 @@ func ProduceCity(c *City, lang string) map[string]interface{} {
 	out["city"] = getNameByLang(c.City.Names, lang)
 	out["continent"] = getNameByLang(c.Continent.Names, lang)
 	out["country"] = getNameByLang(c.Country.Names, lang)
+	out["country_iso"] = c.Country.IsoCode
 
 	out["latitude"] = c.Location.Latitude
 	out["longitude"] = c.Location.Longitude
 	out["accuracyRadius"] = c.Location.AccuracyRadius
+	out["time_zone"] = c.Location.TimeZone
+	out["metro_code"] = c.Location.MetroCode
 
 	out["postCode"] = c.Postal.Code
 
 	out["registeredCountry"] = getNameByLang(c.RegisteredCountry.Names, lang)
+	out["registeredCountry_iso"] = c.RegisteredCountry.IsoCode
+
+	out["representedCountry_iso"] = getNameByLang(c.RepresentedCountry.Names, lang)
+	out["representedCountry_iso"] = c.RepresentedCountry.IsoCode
+	out["representedCountry_type"] = c.RepresentedCountry.Type
+
+	out["subdivisions"] = getNameByLang(c.Subdivisions.Names, lang)
+	out["subdivisions_iso"] = c.Subdivisions.IsoCode
+
+	out["is_anonymous_proxy"] = c.Traits.IsAnonymousProxy
+	out["is_satellite_provider"] = c.Traits.IsSatelliteProvider
 
 	return out
 }
