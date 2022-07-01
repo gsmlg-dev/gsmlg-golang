1e937d6cf4beb780f76f9fb67e1d4118b2b5141e
Jonathan Gao <gsmlg.com@gmail.com>
2022-07-01 11:43:14 +0800

    fix: Fix darwin in hwinfo.

--- hwinfo/darwin.go
+++ hwinfo/darwin.go
@@ -3,7 +3,7 @@ package hwinfo
 import "os/exec"
 
 func DarwinGetInfoString() string {
-	out, err := exec.Command("system_profiler SPHardwareDataType").Output()
+	out, err := exec.Command("system_profiler", "SPHardwareDataType").Output()
 	if err != nil {
 		errorExit(err)
 	}
