7790a023c1f6ce5a62e1995431f9ca19b6d5ece1
Jonathan Gao <gsmlg.com@gmail.com>
2022-07-08 00:45:09 +0800

    fix: Fix.

--- counter/counter.go
+++ counter/counter.go
@@ -20,6 +20,13 @@ func (c *SafeCounter) Inc() {
 	c.mu.Unlock()
 }
 
+func (c *SafeCounter) IncN(n int) {
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.val += n
+	c.mu.Unlock()
+}
+
 // Value returns the current value of the counter for the given key.
 func (c *SafeCounter) Value() int {
 	c.mu.Lock()
