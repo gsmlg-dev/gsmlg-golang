b33093c4c6317f115d0216ea3f988f71df6b1e5b
Jonathan Gao <gsmlg.com@gmail.com>
2022-07-08 00:09:58 +0800

    feat: Add couter for safe count.

---
+++ counter/counter.go
@@ -0,0 +1,29 @@
+package counter
+
+import "sync"
+
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	mu  sync.Mutex
+	val int
+}
+
+func New(start int) SafeCounter {
+	return SafeCounter{val: start}
+}
+
+// Inc increments the counter for the given key.
+func (c *SafeCounter) Inc() {
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	c.val++
+	c.mu.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Value() int {
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer c.mu.Unlock()
+	return c.val
+}
